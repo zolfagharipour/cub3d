3146ba8404a78e8d0ff02ea067fb11f96e60d269
Mohamad Zolfaghari Pour <mzolfagh@c3r8p2.42vienna.com>
2024-06-21 17:00:26 +0200

    scaling isnt working

--- src/minimap.c
+++ src/minimap.c
@@ -6,7 +6,7 @@
 /*   By: mzolfagh <mzolfagh@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/21 14:40:35 by mzolfagh          #+#    #+#             */
-/*   Updated: 2024/06/21 16:14:48 by mzolfagh         ###   ########.fr       */
+/*   Updated: 2024/06/21 16:59:08 by mzolfagh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -77,49 +77,8 @@ int		minimap(t_common *dlist)
 		mmap->minimap[i] = malloc(sizeof(int) * mmap->scale * mmap->raw_length);
 		if (!mmap->minimap[i])
 			return (free_mmap(mmap->minimap));
+		i++;
 	}
 	fill_mmap(dlist, mmap->scale);
 	return (1);
 }
-
-// void	set(t_common *dlist)
-// {
-// 	int arr[5][6] = {
-// 		{1, 1, 1, 1, 1, 1},
-// 		{1, 0, 0, 1, 0, 1},
-// 		{1, 0, 1, 0, 0, 1},
-// 		{1, 1, 0, 0, 1, 1},
-// 		{1, 1, 1, 1, 1, 1}
-// 	};
-// 	t_map mmap;
-// 	mmap.raw_map = malloc(sizeof(int *) * 6);
-// 	for (int k = 0; k < 5; k++)
-// 	{
-// 		mmap.raw_map[k] = malloc(sizeof(int) * 5);
-// 	}
-// 	for (int i = 0; i < 5; i++)
-// 	{
-// 		for (int j = 0; j < 6; j++)
-// 		{
-// 			mmap.raw_map[i][j] = arr[i][j];
-// 		}
-// 		write (1, "\n", 1);
-// 	}
-// 	dlist->map = &mmap;
-// }
-
-// int main()
-// {
-// 	t_common	*dlist;
-
-// 	set(dlist);
-// 	minimap(dlist);
-// 	for (int i = 0; i < 15; i++)
-// 	{
-// 		for (int j = 0; j < 18; j++)
-// 		{
-// 			printf("%d ", dlist->map->minimap[i][j]);
-// 		}
-// 		write (1, "\n", 1);
-// 	}
-// }
